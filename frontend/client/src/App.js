b6a3ac37adff290caf1df9d069447f8c484aa42b
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2021-10-02 20:29:42 -0700

    simple draggable window

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { Component  } from 'react';
+import Draggable from 'react-draggable';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      width: window.innerWidth, 
+      height: window.innerHeight,
+    };
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions = () => {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Draggable
+        defaultClassName="drag"
+        handle=".handle"
+        bounds={{left: 0, top: 0, right: this.state.width - 500, bottom: this.state.height - 300}}>
+          <div>
+            <div className="handle">Drag from here</div>
+            <div className="window-view">This readme is really dragging on...</div>
+          </div>
+        </Draggable>
+      </div>
+    );
+  }
 }
 
 export default App;
